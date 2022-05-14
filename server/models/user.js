2764ef941bf8fee9c86f8c101ac7f3c908a4a3ed
KYLE_HARKIN <harkink1@newpaltz.edu>
2022-05-14 18:55:38 -0400

    npm run dev

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+const users = [
+    {
+        userID: 12345,
+        userName: "cathy123",
+        password: "password"
+    },
+    {
+        userId: 55555,
+        userName: "fredburger2",   
+        password: "password"
+    },
+];
+
+let getUsers = () => users;
+
+module.exports = { getUsers };
+
