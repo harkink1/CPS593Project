b044b1ae15accd311ffe37d0a1ead419f03701bc
KYLE_HARKIN <harkink1@newpaltz.edu>
2022-05-16 15:39:37 -0400

    post models and routes added

--- server/models/post.js
+++ server/models/post.js
@@ -1,3 +1,4 @@
+const { post } = require("../routes/user");
 const con = require("./db_connect");
 
 async function createTable() {
@@ -12,4 +13,40 @@ async function createTable() {
 }
 createTable();
 
-module.exports = {createTable };
+let getPosts = async () => { //get ALL posts from ALL users
+  const sql = `SELECT * FROM posts`;
+  return await con.query(sql);
+};
+
+async function getPost(post) { //get all posts from a user
+  let sql;
+  if(post.userId) { //if the user id matches
+    sql = `SELECT * FROM posts
+      WHERE user_id = ${post.userId}
+    `;
+  } else {
+    throw Error('No Posts!') //if no userid match then no posts!
+  }
+
+  return await con.query(sql);
+}
+
+async function createPost(post) { //create a post
+  const sql = `INSERT INTO posts (contents)
+    VALUES ("${post.contents}")
+  `;
+
+  const insert = await con.query(sql);
+  const newPost = await getPost(post);
+  return newPost[0];
+}
+
+async function deletePost(postId) { //delete a post
+  const sql = `DELETE FROM posts 
+    WHERE post_id = ${postId}
+  `;
+  await con.query(sql);
+ 
+} 
+
+module.exports = { getPosts, createPost, getPost, deletePost, createTable };
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,7 +1,34 @@
 const express = require('express');
-const User = require('../models/post');
+const Post = require('../models/post');
 const router = express.Router();
 
+router
+  .get('/', async (req, res) => {
+    try {
+      const posts = await Post.getPosts();
+      res.send(posts);
+    } catch(err) {
+      res.status(401).send({message: err.message});
+    }
+  })
 
+  .post('/create', async (req, res) => {
+    try {
+      const post = await Post.create(req.body);
+      console.log(post)
+      res.send({...post, contents})
+    } catch(error) {
+      res.status(401).send({message: error.message});
+    }
+  })
+
+  .delete('/delete', async (req, res) => {
+    try {
+      await Post.deletePost(req.body.postId);
+      res.send({success: "Post Deleted"});
+    } catch(error) {
+      res.status(401).send({message: error.message});
+    }
+  })
 
 module.exports = router;
